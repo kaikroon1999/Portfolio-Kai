9bf362db955be6e291d2bc0429cf88343b67cc77
Kai <kaikroon@gmail.com>
2021-01-06 20:46:00 +0100

    php

---
+++ header.php
@@ -0,0 +1,23 @@
+
+<a class="navbar-brand" href="index.html" ><img src="images/logowit.png" width="20%" height="20%"></a>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light" class="center">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="navigatie">
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav nav" role="tablist">
+            <a class="nav-item nav-link" data-toggle="tab" href="#start" id="start-tab" role="tab" aria-controls="start" aria-selected="true"><i class="fa fa-fw fa-home"></i> Welkom <span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link" data-toggle="tab" href="#start2" id="start2-tab" role="tab" aria-controls="start2" aria-selected="true"><i class="fa fa-play"></i> Smart start</a>
+            <a class="nav-item nav-link" data-toggle="tab" href="#business" id="business-tab" role="tab" aria-controls="business" aria-selected="true"><i class="fa fa-handshake-o"></i> Smart business</a>
+            <a class="nav-item nav-link" data-toggle="tab" href="#connection" id="connection-tab" role="tab" aria-controls="connection" aria-selected="true"><i class="fa fa-code"></i> Smart connection</a>
+            <a class="nav-item nav-link" data-toggle="tab" href="#technology" id="technology-tab" role="tab" aria-controls="technology" aria-selected="true"><i class="fa fa-cog"></i> Smart technology</a>
+            <a class="nav-item nav-link" data-toggle="tab" href="#project" id="project-tab" role="tab" aria-controls="project" aria-selected="true"><i class="fa fa-rocket"></i> Smart project</a>
+            <a class="nav-item nav-link" data-toggle="tab" href="#competenties" id="competenties-tab" role="tab" aria-controls="competenties" aria-selected="true"><i class="fa fa-line-chart "></i> Competenties</a>
+            <a class="nav-item nav-link" data-toggle="tab" href="#overmij" id="overmij-tab" role="tab" aria-controls="overmij" aria-selected="true"><i class="fa fa-user"></i> Over mij</a>
+
+        </div>
+    </div>
+    </div>
+</nav>
